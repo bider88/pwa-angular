7cfea371296e925a7996ecbe7614bd7b0274d3da
Irving Didier <bider88@gmail.com>
2018-07-13 00:07:14 -0500

    trabjando en el ngsw-config.json y en el manifest,json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor(
+    private _swUpdate: SwUpdate
+  ) {}
+
+  ngOnInit() {
+    // Con el if, verificamos si el navegador soporta service worker
+    if (this._swUpdate.isEnabled) {
+      // con esto el sw avisa cada vez que hay contenido nuevo disponible
+      this._swUpdate.available.subscribe(
+        () => {
+          window.location.reload();
+        }
+      );
+    }
+  }
 }
